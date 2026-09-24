4ec6fd55ece84bf26de6eaa57a762179be64dd41
vaiper777 <137697705+vaiper777@users.noreply.github.com>
2026-09-24 17:21:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const TABLA_REGISTRO = "REGISTRO";
 const WEBHOOK_URL = "https://mpmm.onrender.com/webhook-mp";
 
 const PLANES = {
-  1: { titulo: "Membresía NIVEL 1", precio: 10 },
+  1: { titulo: "Membresía NIVEL 1", precio: 15 },
   2: { titulo: "Membresía NIVEL 2", precio: 20 },
   3: { titulo: "Membresía NIVEL 3", precio: 30 }
 };
